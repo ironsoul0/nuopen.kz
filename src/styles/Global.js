50cd3f7cc4ee1051338f560eade494e5be51b182
ironsoul0 <timka2609@gmail.com>
2020-02-17 10:15:03 +0600

    add style injection

--- src/styles/Global.js
+++ src/styles/Global.js
@@ -3,6 +3,9 @@ import { createGlobalStyle } from "styled-components";
 import normalize from "./normalize";
 
 const Global = createGlobalStyle`
+  body {
+    font-family: 'Montserrat', sans-serif;
+  }
   ${normalize};
 `;
 
--- src/utils/typography.js
+++ src/utils/typography.js
@@ -14,4 +14,6 @@ const typography = new Typography({
   bodyFontFamily: ["Montserrat", "Georgia", "serif"],
 });
 
+typography.injectStyles();
+
 export default typography;
