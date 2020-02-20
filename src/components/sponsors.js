7c79fec1bb0ca6fe6264fa48305152a80fc1a2d5
ironsoul0 <timka2609@gmail.com>
2020-02-20 17:13:15 +0600

    add sponsors

---
+++ src/components/sponsors.js
@@ -0,0 +1,71 @@
+import React from "react";
+import styled from "styled-components";
+import { useStaticQuery, graphql } from "gatsby";
+import Img from "gatsby-image";
+
+import mixins from "../styles/mixins";
+import { sizes } from "../styles/media";
+
+const Sponsors = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      bts: file(relativePath: { eq: "sponsors/bts.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 150) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      acm: file(relativePath: { eq: "sponsors/acm.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 150) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      kazdream: file(relativePath: { eq: "sponsors/kazdream.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 150) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      okoo: file(relativePath: { eq: "sponsors/okoo.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 150) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `);
+
+  return (
+    <Container>
+      <SponsorImage fluid={data.kazdream.childImageSharp.fluid} width="100" />
+      <SponsorImage fluid={data.bts.childImageSharp.fluid} width="114" />
+      <SponsorImage fluid={data.acm.childImageSharp.fluid} width="82" />
+      <SponsorImage
+        fluid={data.okoo.childImageSharp.fluid}
+        width="75"
+        style={{ marginBottom: "8px" }}
+      />
+    </Container>
+  );
+};
+
+export default Sponsors;
+
+const Container = styled.div`
+  margin: 0 auto;
+  margin-bottom: 30px;
+  justify-content: space-between;
+  width: 100%;
+  max-width: ${sizes.tablet}px;
+
+  ${mixins.rowFlex};
+`;
+
+const SponsorImage = styled(Img)`
+  width: ${props => props.width}px;
+`;
