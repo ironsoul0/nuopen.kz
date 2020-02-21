496bf2d4a1ba15cbe9f734cf4355bce00537a071
ironsoul0 <timka2609@gmail.com>
2020-02-22 00:00:43 +0600

    Start doing faq section

---
+++ src/components/faq.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+import media, { sizes } from "../styles/media";
+import mixins from "../styles/mixins";
+import theme from "../styles/theme";
+
+const FAQ = () => {
+  return (
+    <Container>
+      <Heading>Schedule</Heading>
+      <Subheading>11 April</Subheading>
+    </Container>
+  );
+};
+
+export default FAQ;
