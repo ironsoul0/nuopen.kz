212932db3cb0ca52b6018c5c4381b94d33da6176
ironsoul0 <timka2609@gmail.com>
2020-02-28 23:29:57 +0600

    holy shit, how to make this thing work?

--- src/utils/sr.js
+++ src/utils/sr.js
@@ -2,6 +2,15 @@ import ScrollReveal from "scrollreveal";
 
 // const isSSR = typeof window === "undefined";
 
-const sr = typeof window !== "undefined" && ScrollReveal();
+// const sr = typeof window !== "undefined" && ScrollReveal();
+
+// eslint-disable-next-line import/no-mutable-exports
+let sr = null;
+
+try {
+  sr = ScrollReveal();
+} catch (err) {
+  sr = null;
+}
 
 export default sr;
