f31cea953135160571d13b0c279e843ba2491fd7
ironsoul0 <timka2609@gmail.com>
2020-02-29 14:46:45 +0600

    update favicon

--- src/utils/sr.js
+++ src/utils/sr.js
@@ -1,16 +1,6 @@
 import ScrollReveal from "scrollreveal";
 
-// const isSSR = typeof window === "undefined";
-
-// const sr = typeof window !== "undefined" && ScrollReveal();
-
-// eslint-disable-next-line import/no-mutable-exports
-let sr = null;
-
-try {
-  sr = ScrollReveal();
-} catch (err) {
-  sr = null;
-}
+const isSSR = typeof window === "undefined";
+const sr = isSSR ? null : ScrollReveal();
 
 export default sr;
